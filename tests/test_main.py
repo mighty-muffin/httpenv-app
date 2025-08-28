ada0a4ee3b46c54d7e9b6abf680528027765d1eb
Simon Harvey <simon.harvey.86@gmail.com>
2025-08-28 15:57:06 -0400

    fix: pytest and ruff

--- tests/test_main.py
+++ tests/test_main.py
@@ -4,37 +4,39 @@ from fastapi.testclient import TestClient
 # Setup the TestClient
 client = TestClient(app)
 
+STATUS_OK = 200
+
 
 def test_hello():
     response = client.get('/')
-    assert response.status_code == 200, response.text
+    assert response.status_code == STATUS_OK, response.text
     data = response.json()
     assert data == 'Hello, World!'
 
 
 def test_hello_name():
     response = client.get('/?name=Test')
-    assert response.status_code == 200, response.text
+    assert response.status_code == STATUS_OK, response.text
     data = response.json()
     assert data == 'Hello, Test!'
 
 
 def test_hello_formal():
     response = client.get('/?formal=True')
-    assert response.status_code == 200, response.text
+    assert response.status_code == STATUS_OK, response.text
     data = response.json()
     assert data == 'Good day to you, World.'
 
 
 def test_goodbye():
     response = client.get('/goodbye')
-    assert response.status_code == 200, response.text
+    assert response.status_code == STATUS_OK, response.text
     data = response.json()
     assert data == 'Goodbye, World!'
 
 
 def test_goodbye_name():
     response = client.get('/goodbye?name=Test')
-    assert response.status_code == 200, response.text
+    assert response.status_code == STATUS_OK, response.text
     data = response.json()
     assert data == 'Goodbye, Test!'
