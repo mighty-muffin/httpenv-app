6c976b1462f521dd174ae41681e70eba8f7bcbe3
Simon Tremblay <tremblaysimon@gmail.com>
2025-09-01 16:06:01 -0400

    test hardcoded secret

---
+++ fake_secret.py
@@ -0,0 +1,4 @@
+# Fictive Azure secrets for GitGuardian test
+AZURE_CLIENT_ID = "d6e8f9a0-1234-4b56-8c7d-abcdef123456"
+AZURE_CLIENT_SECRET = "QWERTYuiop1234567890asdfghJKLZXCVBNMqwerty"
+AZURE_TENANT_ID = "12345678-90ab-cdef-1234-567890abcdef"
